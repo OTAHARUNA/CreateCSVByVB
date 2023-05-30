1383d6be60713d2d573c3cf85a8c80cb1ba15bc1
OTAHARUNA <radwimps.love867@gmail.com>
2023-05-30 17:22:26 +0900

    プロジェクト ファイルを追加します。

---
+++ CSVSplitter/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Split = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TargetFullPath = New System.Windows.Forms.TextBox()
+        Me.CreateRow = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.OutputPath = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TargetRef = New System.Windows.Forms.Button()
+        Me.OutputRef = New System.Windows.Forms.Button()
+        Me.HeaderRow = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ChangeRowFlg = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Split
+        '
+        Me.Split.Font = New System.Drawing.Font("MS UI Gothic", 18.0!)
+        Me.Split.Location = New System.Drawing.Point(336, 307)
+        Me.Split.Name = "Split"
+        Me.Split.Size = New System.Drawing.Size(120, 49)
+        Me.Split.TabIndex = 0
+        Me.Split.Text = "CSV作成"
+        Me.Split.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(177, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(132, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ベースとなるファイルフルパス"
+        '
+        'TargetFullPath
+        '
+        Me.TargetFullPath.AllowDrop = True
+        Me.TargetFullPath.Location = New System.Drawing.Point(318, 45)
+        Me.TargetFullPath.Name = "TargetFullPath"
+        Me.TargetFullPath.Size = New System.Drawing.Size(288, 19)
+        Me.TargetFullPath.TabIndex = 2
+        Me.TargetFullPath.Text = "D&Dもできます"
+        Me.TargetFullPath.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'CreateRow
+        '
+        Me.CreateRow.Location = New System.Drawing.Point(318, 103)
+        Me.CreateRow.Name = "CreateRow"
+        Me.CreateRow.Size = New System.Drawing.Size(288, 19)
+        Me.CreateRow.TabIndex = 4
+        Me.CreateRow.Text = "2"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(177, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(103, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "何行作成しますか？"
+        '
+        'OutputPath
+        '
+        Me.OutputPath.Location = New System.Drawing.Point(318, 218)
+        Me.OutputPath.Name = "OutputPath"
+        Me.OutputPath.Size = New System.Drawing.Size(288, 19)
+        Me.OutputPath.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(177, 221)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 12)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "出力先"
+        '
+        'TargetRef
+        '
+        Me.TargetRef.Location = New System.Drawing.Point(632, 43)
+        Me.TargetRef.Name = "TargetRef"
+        Me.TargetRef.Size = New System.Drawing.Size(75, 23)
+        Me.TargetRef.TabIndex = 9
+        Me.TargetRef.Text = "参照"
+        Me.TargetRef.UseVisualStyleBackColor = True
+        '
+        'OutputRef
+        '
+        Me.OutputRef.Location = New System.Drawing.Point(632, 216)
+        Me.OutputRef.Name = "OutputRef"
+        Me.OutputRef.Size = New System.Drawing.Size(75, 23)
+        Me.OutputRef.TabIndex = 10
+        Me.OutputRef.Text = "参照"
+        Me.OutputRef.UseVisualStyleBackColor = True
+        '
+        'HeaderRow
+        '
+        Me.HeaderRow.Location = New System.Drawing.Point(318, 159)
+        Me.HeaderRow.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.HeaderRow.Name = "HeaderRow"
+        Me.HeaderRow.Size = New System.Drawing.Size(288, 19)
+        Me.HeaderRow.TabIndex = 15
+        Me.HeaderRow.Text = "2"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(177, 162)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(141, 12)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "対象のヘッダは何行ですか？"
+        '
+        'ChangeRowFlg
+        '
+        Me.ChangeRowFlg.AutoSize = True
+        Me.ChangeRowFlg.Location = New System.Drawing.Point(577, 263)
+        Me.ChangeRowFlg.Name = "ChangeRowFlg"
+        Me.ChangeRowFlg.Size = New System.Drawing.Size(134, 16)
+        Me.ChangeRowFlg.TabIndex = 16
+        Me.ChangeRowFlg.Text = "毎行内容変更したいか"
+        Me.ChangeRowFlg.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ChangeRowFlg)
+        Me.Controls.Add(Me.HeaderRow)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.OutputRef)
+        Me.Controls.Add(Me.TargetRef)
+        Me.Controls.Add(Me.OutputPath)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.CreateRow)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TargetFullPath)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Split)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Split As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TargetFullPath As TextBox
+    Friend WithEvents CreateRow As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents OutputPath As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TargetRef As Button
+    Friend WithEvents OutputRef As Button
+    Friend WithEvents HeaderRow As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ChangeRowFlg As CheckBox
+End Class
---
+++ CSVSplitter/Form1.vb
@@ -0,0 +1,191 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Collections
+
+Public Class Form1
+#Region "定数"
+#End Region
+#Region "変数"
+#End Region
+
+#Region "もとになるCSVファイルD&D"
+    ''' <summary>
+    ''' もとになるCSVファイルD&D
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub TargetFullPath_DragEnter(ByVal sender As Object, ByVal e As DragEventArgs) Handles TargetFullPath.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+#End Region
+
+#Region "もとになるCSVファイルD&D"
+    ''' <summary>
+    ''' もとになるCSVファイルD&D
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub TargetFullPath_DragDrop(ByVal sender As Object, ByVal e As DragEventArgs) Handles TargetFullPath.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim files As String() = CType(e.Data.GetData(DataFormats.FileDrop), String())
+            ' ドラッグされたファイルに対する処理を行う
+
+            If files.Length > 1 Then
+                'メッセージボックスを表示する
+                MessageBox.Show("ファイルは1ファイルしか取り込めません",
+                        "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error)
+                TargetFullPath.BackColor = Color.Red
+                Exit Sub
+            End If
+
+            ' ファイルのパスを使用して必要な処理を実行する
+            TargetFullPath.Text += files(0) & Environment.NewLine
+
+        End If
+    End Sub
+
+#End Region
+
+#Region "対象CSVファイルの参照ボタン押下"
+    ''' <summary>
+    ''' 対象csvファイルの参照ボタンを押したら
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub TargetReButtonf_Click(sender As Object, e As EventArgs) Handles TargetRef.Click
+        ' ファイルダイアログを開く変数を作成しインスタンス化
+        Dim Dialog As New OpenFileDialog()
+        ' 各パラメータを設定していきます
+        With Dialog
+            ' 最初に表示されるデフォルトのフォルダ名を設定
+            .InitialDirectory = "C:\Users\chopp\OneDrive\デスクトップ\CSV分割"
+            ' 読込むファイルの種類を設定
+            .Filter = "csvファイル" & "|" & "*.csv"
+            ' ファイルダイアログのタイトルを設定
+            .Title = "読み込みたいファイルを選択してください。"
+            ' ファイルダイアログを閉じる前に現在のフォルダを復元するかしないか
+            .RestoreDirectory = True
+        End With
+
+        If Dialog.ShowDialog() = DialogResult.OK Then
+            TargetFullPath.Text = Dialog.FileName
+        End If
+
+    End Sub
+#End Region
+
+#Region "出力先フォルダの参照ボタン押下"
+    ''' <summary>
+    ''' 出力先フォルダの参照ボタンを押したら
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub OutputRefButton_Click(sender As Object, e As EventArgs) Handles OutputRef.Click
+        Dim Dialog As New FolderBrowserDialog()
+
+        ' 各パラメータを設定する
+        With Dialog
+            ' 最初に表示されるデフォルトのフォルダ名を設定
+            .SelectedPath = "C:\Users\chopp\OneDrive\デスクトップ\CSV分割"
+            ' ファイルダイアログのタイトルを設定
+            .Description = "出力先のフォルダを選択してください。"
+            .ShowNewFolderButton = True
+        End With
+
+        If Dialog.ShowDialog() = DialogResult.OK Then
+            ' テキストボックスに入力する
+            OutputPath.Text = Dialog.SelectedPath & "\"
+        End If
+
+    End Sub
+#End Region
+
+#Region "CSV作成"
+    ''' <summary>
+    ''' CSV作成ボタンを押したら
+    ''' </summary>
+    Private Sub SplitButton_Click() Handles Split.Click
+        Dim strTargetFullPath As String = TargetFullPath.Text 'もととなるcsvファイルフルパス
+        Dim strCreateRow As Integer = CreateRow.Text          '作成する行
+        Dim strHeaderRow As String = HeaderRow.Text           'ヘッダの行数
+        Dim strOutputPath As String = OutputPath.Text         '出力先パス
+
+        Dim strFileName As String = Path.GetFileName(strTargetFullPath)  'ファイル名
+
+        Dim lines As String() = File.ReadAllLines(strTargetFullPath, Encoding.UTF8)     'ファイルの中身行を格納
+        Dim strOutputFullPath As String '出力先フルパス
+
+        Try
+            strOutputFullPath = strOutputPath & strFileName.Insert(strFileName.Length - 4, "_" & strCreateRow.ToString)
+            Using writer As New StreamWriter(strOutputFullPath, False, Encoding.UTF8)
+                'ヘッダの書き込み
+                For i As Integer = 0 To strHeaderRow - 1
+                    writer.WriteLine(lines(i))
+                Next
+
+                'チェックがついていなかったらそのまま内容複製
+                If ChangeRowFlg.Checked = False Then
+                    '行の書き込み
+                    For i As Integer = strHeaderRow To strCreateRow
+                        writer.WriteLine(lines(strHeaderRow))
+                    Next
+                Else            'チェックがついていたら末尾に数字をつける
+                    Dim columns As String()
+                    Dim duplicatedData As String = Nothing
+
+                    '行の書き込み
+                    columns = lines(strHeaderRow).Split(","c) ' 列ごとにデータを分割
+                    '行毎
+                    For i As Integer = 1 To strCreateRow
+                        '列毎
+                        For j As Integer = 0 To columns.Length - 1
+                            duplicatedData += columns(j).Trim() & i.ToString()  'データの末尾に行番号を追加して内容を変更する
+
+                            '最後の列でなければ、カンマを追加
+                            If j <> columns.Length - 1 Then
+                                duplicatedData += ","
+                            End If
+                        Next
+
+                        '処理時間懸念の為、毎行毎にファイル書き込みを行う
+                        writer.WriteLine(duplicatedData.ToArray)
+                    Next
+                End If
+
+            End Using
+
+            '処理成功したら初期状態に戻す
+            Init()
+
+            'メッセージボックスを表示する
+            MessageBox.Show("作成完了しました",
+                        "処理完了",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.None)
+
+            '出力先のフォルダを開く
+            System.Diagnostics.Process.Start(strOutputPath)
+        Catch ex As ArithmeticException
+            MessageBox.Show("処理に失敗しました。ご確認ください",
+                        "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error)
+        End Try
+    End Sub
+#End Region
+
+#Region "初期状態に戻す"
+    ''' <summary>
+    ''' 初期状態に戻す
+    ''' </summary>
+    Private Sub Init()
+        CreateRow.BackColor = Color.White
+    End Sub
+#End Region
+End Class
