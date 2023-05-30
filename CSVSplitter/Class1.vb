b7efdbaa61de137cada4ded9f103bd33855f7c2b
OTAHARUNA <radwimps.love867@gmail.com>
2023-05-30 18:28:00 +0900

    リファクタリング：メッセージボックス表示処理メソッド分けようと思ったが、一つにまとめ　まだ残している状態

---
+++ CSVSplitter/Class1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Class1
+#Region "メッセージボックスエラー"
+    ''' <summary>
+    ''' メッセージボックス
+    ''' </summary>
+    Public Shared Sub ShowMessageBox(message As String, strStatus As String)
+        Dim strTitle As String = Nothing
+        Dim icon As MessageBoxIcon
+
+        If strStatus = "Error" Then
+            strTitle = "Error"
+            icon = MessageBoxIcon.Error
+        ElseIf strStatus = "Result" Then
+            strTitle = "処理結果"
+            icon = MessageBoxIcon.None
+        ElseIf strStatus = "注意" Then
+            strTitle = "Error"
+            icon = MessageBoxIcon.Exclamation
+        End If
+
+        'メッセージボックス表示
+        MessageBox.Show(message,
+                        strTitle,
+                        MessageBoxButtons.OK,
+                        icon)
+    End Sub
+#End Region
+
+#Region "メッセージボックス処理結果"
+    ''' <summary>
+    ''' メッセージボックス
+    ''' </summary>
+    Public Shared Sub ShowMessageBoxResult(message As String)
+        MessageBox.Show(message,
+                        "処理結果",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.None)
+    End Sub
+#End Region
+
+#Region "メッセージボックス注意"
+    ''' <summary>
+    ''' メッセージボックス
+    ''' </summary>
+    Public Shared Sub ShowMessageBoxAttention(message As String)
+        MessageBox.Show(message,
+                        "注意",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation)
+    End Sub
+#End Region
+End Class
--- CSVSplitter/Form1.vb
+++ CSVSplitter/Form1.vb
@@ -6,6 +6,7 @@ Public Class Form1
 #Region "定数"
 #End Region
 #Region "変数"
+
 #End Region
 
 #Region "もとになるCSVファイルD&D"
@@ -36,11 +37,11 @@ Public Class Form1
 
             If files.Length > 1 Then
                 'メッセージボックスを表示する
-                MessageBox.Show("ファイルは1ファイルしか取り込めません",
-                        "Error",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Error)
+                Class1.ShowMessageBox("ファイルは1ファイルしか取り込めません", "Error")
+
+                '問題がある箇所背景赤に変更
                 TargetFullPath.BackColor = Color.Red
+
                 Exit Sub
             End If
 
@@ -111,72 +112,88 @@ Public Class Form1
     ''' CSV作成ボタンを押したら
     ''' </summary>
     Private Sub SplitButton_Click() Handles Split.Click
-        Dim strTargetFullPath As String = TargetFullPath.Text 'もととなるcsvファイルフルパス
-        Dim strCreateRow As Integer = CreateRow.Text          '作成する行
-        Dim strHeaderRow As String = HeaderRow.Text           'ヘッダの行数
-        Dim strOutputPath As String = OutputPath.Text         '出力先パス
+        Dim strTargetFullPath As String = TargetFullPath.Text    'もととなるcsvファイルフルパス
+        Dim strCreateRow As Integer = CreateRow.Text        '作成する行
+        Dim strHeaderRow As Integer = HeaderRow.Text          'ヘッダの行数
+        Dim strOutputPath As String = OutputPath.Text       '出力先パス
+
 
         Dim strFileName As String = Path.GetFileName(strTargetFullPath)  'ファイル名
 
         Dim lines As String() = File.ReadAllLines(strTargetFullPath, Encoding.UTF8)     'ファイルの中身行を格納
         Dim strOutputFullPath As String '出力先フルパス
 
+
         Try
+            '作成するファイル名は末尾に作成行数を記載する
             strOutputFullPath = strOutputPath & strFileName.Insert(strFileName.Length - 4, "_" & strCreateRow.ToString)
+
             Using writer As New StreamWriter(strOutputFullPath, False, Encoding.UTF8)
                 'ヘッダの書き込み
                 For i As Integer = 0 To strHeaderRow - 1
                     writer.WriteLine(lines(i))
                 Next
 
-                'チェックがついていなかったらそのまま内容複製
-                If ChangeRowFlg.Checked = False Then
-                    '行の書き込み
-                    For i As Integer = strHeaderRow To strCreateRow
-                        writer.WriteLine(lines(strHeaderRow))
-                    Next
-                Else            'チェックがついていたら末尾に数字をつける
-                    Dim columns As String()
-                    Dim duplicatedData As String = Nothing
-
-                    '行の書き込み
-                    columns = lines(strHeaderRow).Split(","c) ' 列ごとにデータを分割
-                    '行毎
-                    For i As Integer = 1 To strCreateRow
-                        '列毎
-                        For j As Integer = 0 To columns.Length - 1
-                            duplicatedData += columns(j).Trim() & i.ToString()  'データの末尾に行番号を追加して内容を変更する
-
-                            '最後の列でなければ、カンマを追加
-                            If j <> columns.Length - 1 Then
-                                duplicatedData += ","
-                            End If
-                        Next
-
-                        '処理時間懸念の為、毎行毎にファイル書き込みを行う
-                        writer.WriteLine(duplicatedData.ToArray)
-                    Next
-                End If
-
+                'データ書き込み
+                MakeDataRow(strHeaderRow, strCreateRow, strOutputFullPath, lines, writer)
             End Using
 
             '処理成功したら初期状態に戻す
             Init()
 
             'メッセージボックスを表示する
-            MessageBox.Show("作成完了しました",
-                        "処理完了",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.None)
+            Class1.ShowMessageBox("作成完了しました", "Result")
 
             '出力先のフォルダを開く
-            System.Diagnostics.Process.Start(strOutputPath)
+            'System.Diagnostics.Process.Start(strOutputPath)
+            '出力先したファイルを開く 上記は残すか考え中
+            Process.Start(strOutputFullPath)
+
         Catch ex As ArithmeticException
-            MessageBox.Show("処理に失敗しました。ご確認ください",
-                        "Error",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Error)
+            Class1.ShowMessageBox("処理に失敗しました。ご確認ください", "Error")
         End Try
+    End Sub
+#End Region
+
+#Region "データ作成"
+    ''' <summary>
+    ''' データ作成
+    ''' <param name="strHeaderRow"></param>
+    ''' <param name="strCreateRow"></param>
+    ''' <param name="strOutputFullPath"></param>
+    ''' <param name="lines"></param>
+    ''' <param name="writer"></param>
+    ''' </summary>
+    Private Sub MakeDataRow(strHeaderRow As Integer, strCreateRow As Integer, strOutputFullPath As String, lines As String(), writer As StreamWriter)
+        'チェックがついていなかったらそのまま内容複製
+        If ChangeRowFlg.Checked = False Then
+            '行の書き込み
+            For i As Integer = strHeaderRow To strCreateRow
+                writer.WriteLine(lines(strHeaderRow))
+            Next
+        Else            'チェックがついていたら末尾に数字をつける
+            Dim columns As String()
+            Dim duplicatedData As String
+
+            '行の書き込み
+            columns = lines(strHeaderRow).Split(","c) ' 列ごとにデータを分割
+            '行毎
+            For i As Integer = 1 To strCreateRow
+                duplicatedData = Nothing              '行を読み込むごとに初期化
+                '列毎
+                For j As Integer = 0 To columns.Length - 1
+                    duplicatedData += columns(j).Trim() & i.ToString()  'データの末尾に行番号を追加して内容を変更する
+
+                    '最後の列でなければ、カンマを追加
+                    If j <> columns.Length - 1 Then
+                        duplicatedData += ","
+                    End If
+                Next
+
+                '処理時間懸念の為、毎行毎にファイル書き込みを行う
+                writer.WriteLine(duplicatedData.ToArray)
+            Next
+        End If
     End Sub
 #End Region
 
